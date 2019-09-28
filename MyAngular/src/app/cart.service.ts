e239b8852b8421862c8c07b755a13d215b611d2a
Li YeHui <liyehui@Lis-MacBook-Pro.local>
2019-09-28 12:46:28 +0900

    My first Commit 2019/09/28 12:46 JST

---
+++ MyAngular/src/app/cart.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable(
+  {providedIn: 'root'}
+)
+export class CartService {
+
+  items=[];
+
+  addToCart(products){
+    this.items.push(products);
+  }
+  getItems(){
+    return this.items;
+  }
+  clearCart(){
+    this.items=[];
+    return this.items;
+  }
+  constructor() { }
+
+}
